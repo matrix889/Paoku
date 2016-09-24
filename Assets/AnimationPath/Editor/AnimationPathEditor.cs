2b9c5b517785cb8b2ea9f9ef3c18ef41988432d6
matrix889 <397470071@qq.com>
2016-09-25 02:32:54 +0800

    A Unity Animation Path Preview Unity 动画路径预览工具，来源：https://github.com/akof1314/AnimationPath

---
+++ Assets/AnimationPath/Editor/AnimationPathEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AnimationPath))]
+public class AnimationPathEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        AnimationPathSceneUI.OnInspectorGUI((target as MonoBehaviour).gameObject);
+    }
+
+    private void OnSceneGUI()
+    {
+        AnimationPathSceneUI.OnSceneGUI();
+    }
+}
